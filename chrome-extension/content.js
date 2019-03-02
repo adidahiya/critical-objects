449b3e4272d9a42fbd276f4f8a0d7f66e278998e
Adi Dahiya <adi.dahiya14@gmail.com>
2019-03-02 14:03:17 -0500

    remove changeColor, add content script

---
+++ chrome-extension/content.js
@@ -0,0 +1,5 @@
+/**
+ * Content script, run on all pages
+ */
+
+console.log("yay i'm here");
--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -2,28 +2,6 @@
  * Popup script
  */
 
-let changeColorButton = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', (data) => {
-    changeColorButton.style.backgroundColor = data.color;
-    changeColorButton.setAttribute('value', data.color);
-});
-
-changeColorButton.onclick = (element) => {
-    let color = element.target.value;
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, (tabs) => {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {
-                code: `document.body.style.backgroundColor = '${color}';`
-            },
-        );
-    });
-};
-
 let reloadButton = document.getElementById('reload');
 reloadButton.onclick = () => {
     chrome.runtime.reload();
