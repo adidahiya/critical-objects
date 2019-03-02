fd257c6f9807d31b7816e23a78f575f316cf94ec
Adi Dahiya <adi.dahiya14@gmail.com>
2019-03-01 23:04:32 -0500

    chrome extension scaffolding

---
+++ chrome-extension/popup.js
@@ -0,0 +1,10 @@
+/**
+ * Popup script
+ */
+
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function (data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+});
