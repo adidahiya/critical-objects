a9f495e7ba3e9f9034622591ac20b158a4291ee4
Adi Dahiya <adi.dahiya14@gmail.com>
2019-03-03 17:09:15 -0500

    Better tracking of active tab URL

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -9,33 +9,18 @@ let serial;
 let isConnected = false;
 let synth;
 
-// other random state, should be better encapsulated
-
-/** @type {String} */
-let lastTabNavigation;
+// only stores tabs that have completed loading their URLs
+let activeTab = {
+    /** @type {Number} */
+    id: undefined,
+    /** @type {String} */
+    url: undefined,
+}
 
 /** @type {String} good, ok, bad */
 let humanBehaviorState = "good";
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([
-            // on developer.chrome.com pages, show the extension's icon in the chrome menu bar in the top right
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: {
-                            hostEquals: 'developer.chrome.com'
-                        },
-                    }),
-                ],
-                actions: [
-                    new chrome.declarativeContent.ShowPageAction(),
-                ],
-            },
-        ]);
-    });
-
     chrome.storage.local.set({
         mute: false,
     });
@@ -54,14 +39,20 @@ setInterval(() => {
     });
 }, 1000);
 
-chrome.webNavigation.onCompleted.addListener(() => {
-    console.log("web navigation");
-});
+// typically used to block or redirect requests, but we use this event to inspect if the right resources
+// (analytics and tracking pixels) are about to be requested for the active page
+// chrome.webRequest.onBeforeRequest.addListener(() => {
+// });
 
+// https://developer.chrome.com/extensions/tabs#event-onUpdated
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.url !== undefined) {
-        console.log("tab updated", changeInfo.url);
-        lastTabNavigation = changeInfo.url;
+    if (!tab.active) {
+        return;
+    }
+
+    if (changeInfo.url !== undefined && changeInfo.status === "complete") {
+        console.log("active tab completed loading", changeInfo.url);
+        updateActiveTab(tabId, changeInfo.url);
 
         if (isGoodWebsite()) {
             if (humanBehaviorState === "bad") {
@@ -77,6 +68,40 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     }
 });
 
+// https://developer.chrome.com/extensions/tabs#event-onActivated
+chrome.tabs.onActivated.addListener(({ tabId }) => {
+    chrome.tabs.get(tabId, (tab) => {
+        updateActiveTab(tabId, tab.url);
+    });
+});
+
+chrome.webNavigation.onCommitted.addListener(({ tabId, url, frameId }) => {
+    // ignore subframes
+    if (frameId > 0) {
+        return;
+    }
+
+    // we only care about navigation in the active tab, since inactive tabs will have their urls queried
+    // upon tabs.onActivated
+    if (tabId === activeTab.id) {
+        updateActiveTab(tabId, url);
+    }
+});
+
+function updateActiveTab(tabId,  url) {
+    if (activeTab.id === tabId && activeTab.url === url) {
+        return;
+    }
+
+    if (url.startsWith("chrome://")) {
+        return;
+    }
+
+    console.log("active tab updated", tabId, url);
+    activeTab.id = tabId;
+    activeTab.url = url
+}
+
 function playStatusBeep() {
     if (synth === undefined) {
         synth = new Tone.Synth().toMaster();
@@ -89,10 +114,10 @@ function playStatusBeep() {
 }
 
 function isGoodWebsite() {
-    const s = lastTabNavigation;
-    return s.startsWith("https://www.facebook")
-        || s.startsWith("https://www.instagram")
-        || s.startsWith("https://web.whatsapp");
+    const { url } = activeTab;
+    return url.startsWith("https://www.facebook")
+        || url.startsWith("https://www.instagram")
+        || url.startsWith("https://web.whatsapp");
 }
 
 function openSerialConnection(callback) {
