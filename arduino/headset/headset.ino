533a60880c861b3d02a99dd1bddcf26ff36bfdee
Adi Dahiya <adi.dahiya14@gmail.com>
2019-03-03 19:19:28 -0500

    Add servo control to headset.ino (#1)

--- arduino/headset/headset.ino
+++ arduino/headset/headset.ino
@@ -1,23 +1,46 @@
+#include <Servo.h>
+
 #define HUMAN_BEHAVIOR_DATA_PREFIX "humanBehaviorState:"
 #define GOOD_STATE "good"
+
+//set up gate control servo
+Servo gateControl;
+const int servoPin = 4;
+const int position1 = 5;
+const int position2 = 80;
 
 void setup() {
     Serial.begin(9600);
     Serial.write("hello world");
     pinMode(LED_BUILTIN, OUTPUT);
+
+    gateControl.attach(servoPin);
+    gateControl.write(position1);
+
 }
 
-bool isBlinking = false;
+bool isFeeding = false;
+unsigned long timerAmt = 0;
+unsigned long openAmt = 500;
+
+
+String formerData = "humanBehaviorState:neutral";
+String data = "humanBehaviorState:neutral";
 
 void loop() {
     if (Serial.available() > 0) {
-        String data = Serial.readStringUntil('\n');
+        data = Serial.readStringUntil('\n');
 
         Serial.println(data);
         if (data == "humanBehaviorState:good") {
-            isBlinking = false;
+
+            //open the gate and 'start' the timer
+            gateControl.write(position2);
+            timerAmt = millis();
         } else if (data == "humanBehaviorState:bad") {
-            isBlinking = true;
+          //make sure the gate closes immediately if state switches to "bad"
+          servo.write(position1);
+          timerAmt = 0;
         }
 
         // if (data.startsWith(HUMAN_BEHAVIOR_DATA_PREFIX)) {
@@ -28,12 +51,9 @@ void loop() {
         // }
     }
 
-    if (isBlinking) {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(50);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(50);
-    } else {
-        digitalWrite(LED_BUILTIN, LOW);
+     //once the gate has been open long enough, then shut it again
+    if(millis() - timerAmt >= openAmt){
+      servo.write(position1);
+      timerAmt = 0;
     }
 }
